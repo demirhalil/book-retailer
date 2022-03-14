f3c376d786144f5a54ed5ff4b691a0aaa914939b
Halil Demir <halil.demir@rewe-digital.com>
2022-03-14 23:30:30 +0100

    implemented mongo database configuration and swagger configuration

---
+++ src/main/java/com/book/configuration/BookServiceMongoConfiguration.java
@@ -0,0 +1,18 @@
+package com.book.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class BookServiceMongoConfiguration extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "bookservice";
+    }
+
+    @Override
+    protected boolean autoIndexCreation() {
+        return true;
+    }
+}
