1b496c7f033f4b26be3f09ea00bd900a8248efaa
Halil Demir <halil.demir@rewe-digital.com>
2022-03-16 23:39:10 +0100

    implementation of customer integration tests are done

--- src/it/java/com/book/CustomerIT.java
+++ src/it/java/com/book/CustomerIT.java
@@ -4,12 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.book.domain.Customer;
+import com.book.domain.Order;
 import com.book.repository.CustomerRepository;
+import com.book.repository.OrderRepository;
 import com.book.util.TestUtil;
+import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import java.time.LocalDateTime;
+import java.util.Objects;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +21,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
@@ -31,24 +36,19 @@ public class CustomerIT {
     private TestRestTemplate restTemplate;
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private OrderRepository orderRepository;
     @LocalServerPort
     private Integer port;
-
-    @BeforeEach
-    void setUp() {
-        customerRepository.deleteAll();
-    }
-
-    @AfterEach
-    void tearDown() {
-        customerRepository.deleteAll();
-    }
+    private URI uri;
+    private Customer customer;
 
     @Test
     void testAddCustomer() throws URISyntaxException {
         // Given
-        URI uri = new URI(BASE_URL);
-        Customer customer = TestUtil.createCustomer("customer-name", "customer-lastname");
+        customerRepository.deleteAll();
+        uri = new URI(BASE_URL);
+        customer = TestUtil.createCustomer("customer-name", "customer-lastname");
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(getBearerToken());
         HttpEntity<Customer> request = new HttpEntity<>(customer, headers);
@@ -62,6 +62,39 @@ public class CustomerIT {
         assertNotNull(result.getBody().getId());
         assertEquals(customer.getName(),result.getBody().getName());
         assertEquals(customer.getLastName(),result.getBody().getLastName());
+
+        customerRepository.deleteAll();
+        customer = null;
+        uri = null;
+    }
+
+    @Test
+    void testGetOrdersByCustomerId() throws URISyntaxException {
+        // Given
+        orderRepository.deleteAll();
+        customerRepository.deleteAll();
+        uri = new URI(BASE_URL);
+        customer = TestUtil.createCustomer("customer-name", "customer-lastname");
+        customerRepository.save(customer);
+        Customer existCustomer = customerRepository.findAll().size() == 1 ?
+            customerRepository.findAll().get(0) : null;
+        assert existCustomer != null;
+        Order order = new Order(null, "book-id", existCustomer.getId(), 3, LocalDateTime.now(),
+            LocalDateTime.now().plusDays(10),
+            BigDecimal.valueOf(39));
+        orderRepository.save(order);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(getBearerToken());
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+
+        // When
+        final ResponseEntity<Order[]> response = this.restTemplate.exchange(uri + "/" + existCustomer.getId(),
+            HttpMethod.GET, requestEntity, Order[].class);
+
+        // Then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, Objects.requireNonNull(response.getBody()).length);
+        assertEquals(existCustomer.getId(), response.getBody()[0].getCustomerId());
     }
 
     private String getBearerToken() throws URISyntaxException {
@@ -69,8 +102,8 @@ public class CustomerIT {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request = new HttpEntity<>(body,headers);
-        final ResponseEntity<String> stringResponseEntity = this.restTemplate.postForEntity(new URI(SECURITY_URL),
+        final ResponseEntity<String> response = this.restTemplate.postForEntity(new URI(SECURITY_URL),
             request, String.class);
-        return stringResponseEntity.getBody().split(":")[1].split("[},\"]")[1];
+        return response.getBody();
     }
 }
